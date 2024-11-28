b30bedf963678b7eb56e940e9d27d5730237a056
Mark Derman <mark@soulv.co.za>
2024-11-28 13:29:41 +0200

    More tests

--- Source/Tests.Odin/Email/Office365/Office365EmailSenderTests.cs
+++ Source/Tests.Odin/Email/Office365/Office365EmailSenderTests.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Azure.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Graph;
@@ -12,7 +11,7 @@ namespace Tests.Odin.Email.Office365;
 
 [TestFixture]
 //[Ignore("Manual testing")]
-public class Office365EmailSenderTests: IntegrationTest
+public class Office365EmailSenderTests : IntegrationTest
 {
     private Office365EmailSender _sut;
     private string _toTestEmail;
@@ -23,22 +22,22 @@ public class Office365EmailSenderTests: IntegrationTest
     {
         IConfiguration config = AppFactory.GetConfiguration();
         IConfigurationSection office365Section = config.GetRequiredSection("Email-Office365");
-        IConfigurationSection  office365Options =  office365Section.GetRequiredSection("Options");
+        IConfigurationSection office365Options = office365Section.GetRequiredSection("Options");
 
         _toTestEmail = config["Email-TestToAddress"];
         _fromTestEmail = office365Section["UserId"];
         Office365Options options = new Office365Options();
         office365Options.Bind(options);
-        
+
         ClientSecretCredentialOptions credentialOptions = new ClientSecretCredentialOptions
         {
             AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
         };
 
         ClientSecretCredential clientSecretCredential = new ClientSecretCredential(
-            options.MicrosoftGraphClientSecretCredentials!.TenantId, 
+            options.MicrosoftGraphClientSecretCredentials!.TenantId,
             options.MicrosoftGraphClientSecretCredentials.ClientId,
-            options.MicrosoftGraphClientSecretCredentials.ClientSecret, 
+            options.MicrosoftGraphClientSecretCredentials.ClientSecret,
             credentialOptions);
 
         GraphServiceClient graphClient = new GraphServiceClient(clientSecretCredential);
@@ -47,41 +46,61 @@ public class Office365EmailSenderTests: IntegrationTest
     }
 
     [Test]
-    public async Task SendEmail_Works()
+    [TestCase("1-Tag")]
+    [TestCase("Null-Tags")]
+    [TestCase("Empty-Tags")]
+    [TestCase("Many-Tags")]
+    [TestCase("1-Attachment")]
+    [TestCase("2-Attachments")]
+    [TestCase("Plain-Text-Body")]
+    public async Task Send_various_emails(string testCase)
     {
         EmailMessage email = new EmailMessage()
         {
-            Subject = "Test Office365 08",
-            Body = "Hello World (Test 08)",
+            Subject = "Office365 test case: ",
+            Body = $"<p>{testCase}</p>",
             To = [new EmailAddress(_toTestEmail, "Test To Name")],
             From = new EmailAddress(_fromTestEmail, "Test From Name"),
-            Tags = ["Dev"]
+            IsHtml = true
         };
-
-        Outcome<string> outcome = await _sut.SendEmail(email);
-
-        TestContext.Progress.WriteLine(outcome.MessagesToString());
-        
-        Assert.That(outcome.Success);
-        
-    }
-
-    [Test]
-    public async Task SendEmail_With_Attachment()
-    {
-        EmailMessage email = new EmailMessage
+        email.Subject += testCase;
+        switch (testCase)
         {
-            Subject = "Test with attachments 01",
-            Body = "Hello, here is a photo 01",
-            To = new EmailAddressCollection(_toTestEmail),
-            Attachments = [new Attachment("file.txt", new MemoryStream("This is a text file"u8.ToArray()), "text/plain")]
-        };
-        
+            case "1-Tag":
+                email.Tags = ["Dev"];
+                break;
+            case "Many-Tags":
+                email.Tags = ["QA", "Dev", "Outgoing Payments"];
+                break;
+            case "Null-Tags":
+                email.Tags = null!;
+                break;
+            case "Empty-Tags":
+                email.Tags = new List<string>();
+                break;
+            case "1-Attachment":
+                email.Attachments =
+                    [new Attachment("file.txt", new MemoryStream("This is a text file"u8.ToArray()), "text/plain")];
+                break;
+            case "2-Attachments":
+                email.Attachments =
+                [
+                    new Attachment("file1.txt", new MemoryStream("This is a text file 1"u8.ToArray()), "text/plain"),
+                    new Attachment("file2.txt", new MemoryStream("This is a text file 2"u8.ToArray()), "text/plain")
+                ];
+                break;
+            case "Plain-Text-Body":
+                email.IsHtml = false;
+                email.Body = testCase;
+                break;
+            default:
+                Assert.Fail($"Case {testCase} not implemented");
+                break;
+        }
+
         Outcome<string> outcome = await _sut.SendEmail(email);
 
-        TestContext.Progress.WriteLine(outcome.MessagesToString());
-        
+        TestContext.WriteLine(outcome.MessagesToString());
         Assert.That(outcome.Success);
     }
-
 }
