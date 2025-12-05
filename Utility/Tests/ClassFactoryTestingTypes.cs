a90073bfd02f80bb5166ab8e1e3f5320c9936f8d
Mark Derman <mark@soulv.co.za>
2025-12-05 15:53:28 +0200

    Tests fix and new TestsOnly solution

--- Utility/Tests/ClassFactoryTestingTypes.cs
+++ Utility/Tests/ClassFactoryTestingTypes.cs
@@ -5,8 +5,9 @@ public interface Interface1
     public string Member1 { get; set; }
 }
 
-public interface Inherited2 : Interface1
+public class Inherited2 : Interface1
 {
+    public string Member1 { get; set; }
     public string Member2 { get; set; }
 }
 
--- Utility/Tests/ClassFactoryTests.cs
+++ Utility/Tests/ClassFactoryTests.cs
@@ -8,7 +8,19 @@ namespace Tests.Odin.Utility
     public sealed class ClassFactoryTests
     {
         [Test]
-        public void TryCreate_by_type()
+        public void Create_class_by_type()
+        {
+            ClassFactory activator = new ClassFactory();
+            ResultValue<Class3> result = activator.TryCreate<Class3>(typeof(Class3));
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Value, Is.Not.Null);
+            Assert.That(result.Value, Is.InstanceOf<Class3>());
+        }
+        
+        [Test]
+        public void Create_inherited_class_by_interface_type()
         {
             ClassFactory activator = new ClassFactory();
             ResultValue<Interface1> result = activator.TryCreate<Interface1>(typeof(Inherited2));
@@ -20,7 +32,7 @@ namespace Tests.Odin.Utility
         }
         
         [Test]
-        public void TryCreate_by_typename()
+        public void Create_class_by_typename()
         {
             ClassFactory activator = new ClassFactory();
             ResultValue<Class3> result = activator.TryCreate<Class3>("Tests.Odin.Utility.Class3");
@@ -29,6 +41,18 @@ namespace Tests.Odin.Utility
             Assert.That(result.Success, Is.True);
             Assert.That(result.Value, Is.Not.Null);
             Assert.That(result.Value, Is.InstanceOf<Class3>());
+        }
+        
+        [Test]
+        public void Create_inherited_class_by_typename()
+        {
+            ClassFactory activator = new ClassFactory();
+            ResultValue<Interface1> result = activator.TryCreate<Interface1>("Tests.Odin.Utility.Inherited2");
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Value, Is.Not.Null);
+            Assert.That(result.Value, Is.InstanceOf<Interface1>());
         }
     }
 }
